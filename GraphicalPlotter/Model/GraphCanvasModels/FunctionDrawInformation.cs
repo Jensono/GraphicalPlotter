55c0feb5fad38a60d8557b16b876f8c7147ee1f7
Jensono <jensono@gmx.at>
2023-05-15 17:53:01 +0200

    cop styling

--- GraphicalPlotter/Model/GraphCanvasModels/FunctionDrawInformation.cs
+++ GraphicalPlotter/Model/GraphCanvasModels/FunctionDrawInformation.cs
@@ -1,19 +1,41 @@
-﻿
-
-namespace GraphicalPlotter
+﻿namespace GraphicalPlotter
 {
+    using System;
     using System.Collections.Generic;
     using System.Windows.Media;
 
     public class FunctionDrawInformation
     {
-        public List<CanvasPixel> CanvasPixels { get; set; }
-        public Color FunctionColor { get; set; }
+        private List<CanvasPixel> canvasPixels;
+
+        private Color functionColor;
 
         public FunctionDrawInformation(List<CanvasPixel> canvasPixel, Color functionColor)
         {
             this.FunctionColor = functionColor;
             this.CanvasPixels = canvasPixel;
+        }
+
+        public Color FunctionColor
+        {
+            get { return functionColor; }
+            set { functionColor = value; }
+        }
+
+        public List<CanvasPixel> CanvasPixels
+        {
+            get
+            {
+                return this.canvasPixels;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException($"{nameof(this.CanvasPixels)} can not be null!");
+                }
+                this.canvasPixels = value;
+            }
         }
     }
 }
